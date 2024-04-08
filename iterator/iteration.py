7143f0c06c6a4cd70b5544e3dc71e3dd007b45cc
Natan Cordeiro <98246249+natancordeiro@users.noreply.github.com>
2024-04-08 01:10:02 -0300

    erro de digitação

--- iterator/iteration.py
+++ iterator/iteration.py
@@ -1,4 +1,4 @@
-dsfrom selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait

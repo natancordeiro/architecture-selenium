b95f28c9b29ea072ca58085457c580a1c0a13cfe
Natan Cordeiro <98246249+natancordeiro@users.noreply.github.com>
2024-05-29 17:40:32 -0300

    Modifiquei a chamada do log

--- bot.py
+++ bot.py
@@ -17,24 +17,8 @@ class Bot(Interation):
         Args:
             log_file (bool): Define se os registros serão salvos em um arquivo de log (padrão: True).
         """
-        if log_file:
-            data = datetime.now().strftime("%d-%m-%Y")
-            nome_arquivo = f"{data}_logs.log"
-            caminho_pasta_logs = "logs"
-            self.caminho_completo_arquivo = os.path.join(caminho_pasta_logs, nome_arquivo)
-            os.makedirs(caminho_pasta_logs, exist_ok=True)
-            handle = [logging.FileHandler(self.caminho_completo_arquivo, 'a'), logging.StreamHandler()]
-        else:
-            handle = None
-
-        logging.basicConfig(
-            level=logging.INFO,
-            encoding='utf-8',
-            format='%(asctime)s - %(levelname)s - %(message)s - %(filename)s:%(lineno)d',
-            datefmt='%d-%m-%Y %H:%M:%S',
-            handlers=handle
-        )
-
+        logger = setup_logging(to_file=True)
+        
         self.driver = Driver(
             browser='chrome',
             headless=False,
